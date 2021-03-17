23b1d753b60546622e2483617fad7ed6b2605e53
61160051 <71600144+ruangkhao2312@users.noreply.github.com>
2021-03-17 20:06:30 +0700

    Update line.php

--- line.php
+++ line.php
@@ -4,6 +4,12 @@ $API_URL = 'https://api.line.me/v2/bot/message/reply';
 $ACCESS_TOKEN = 'N8s8BG5qnDeimcfC+9IuMUiNHNNCmcWg0IRn5RPcG6+Wzudh9uX1wtqhqxxEOHo7koElbs3QbAyari1IQlto6buynntQINZU7k/Gf8bszmAwSi6CDkw1YQJT3p1PQvKlkr5x9uUtddGNKe5LhNp9bwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
+<?php
+
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$ACCESS_TOKEN = '5lYmrTl8D8uUy7bTSJAfNdaqbzJz3+/dYesdRseiXxrf245oxLeKoHUtXklBbz8hlAHr6z/8C0uBIDNQsdSejo+uQNvaEySx3/oTKqEC5B0SDclbwjEqPHFcwjAR0/xXD6Na64tKupkfw+V3mv7htQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
@@ -20,68 +26,58 @@ if ( sizeof($request_array['events']) > 0 )
    
    if( $event['message']['type'] == 'text' )
    {
-		$text = $event['message']['text'];
-		
-	   	if($text == "ชื่อ" || $text == "ชื่ออะไร" || $text == "ชื่ออะไรครับ"|| $text == "ชื่ออะไรคะ"){
-			$reply_message = 'ชื่อของฉันคือ BOTCAT';
-		}
-	   
-	   	if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
-		   $url = 'https://covid19.th-stat.com/api/open/today';
-		   $ch = curl_init($url);
-		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		   $result = curl_exec($ch);
-		   curl_close($ch);   
-		   
-		   $obj = json_decode($result);
-		   
-		   //$reply_message = $result;
-		   $reply_message = 'ติดเชื้อสะสมโว้ยยย!!! '. $obj->{'Confirmed'} .' คน รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
-	
-	        }	
-	   
-	   if($text =="@บอท ขอรหัสนิสิตของผู้พัฒนา ส่งไปที่ https://linebot.kantit.com/stuid.php"){
-	    	  $url = 'https://linebot.kantit.com/stuid.php';
-		   $ch = curl_init($url);
-		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		   $result = curl_exec($ch);
-		   curl_close($ch);   		   
-		   $obj = json_decode($result);		   
-		   $reply_message = $result;
-		   //$reply_message = 'ผลการบันทึกข้อมูล'. $obj->{'status'} .' และ '.$obj->{'data'} . ' OK!';
-	   }
-	   
-	   if($text =="@บอท ขอรายชื่อนิสิตที่ส่งงาน LineBoT"){
-	    	   $url = 'https://linebot.kantit.com/list.php';
-		   
-		   $reply_message = file_get_contents($url);   // Get request content
-		   
-		   //$request_array = json_decode($request, true);   // Decode JSON to Array
-		   
-		   //$result = file_get_contents($url);		   
-		   //$obj = json_decode($result);
-		   //$reply_message = 'มีส่งงาน '. $obj->{'Confirmed'} .' คน ได้แก่...';
-		   //$reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} .' คน รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
-		   $reply_message = "โปรดรอสักครู่....";
-	   }
-	   
-	     if($text =="@บอท ขอที่อยู่มทร.หน่อยจิ"){
-	    	   $url = 'https://www.google.com/maps/place/Phra+Nakhon+Si+Ayutthaya/@14.3935691,100.237786,10z/data=!3m1!4b1!4m8!1m2!2m1!1smaps+google!3m4!1s0x30e2736f5bfd8f7f:0x1019237450c4860!8m2!3d14.3532128!4d100.5689599';
-		   
-		   $reply_message = $url;   // Get request content
+  $text = $event['message']['text'];
+  
+     if($text == "ชื่อ" || $text == "ชื่ออะไร" || $text == "ชื่ออะไรครับ"|| $text == "ชื่ออะไรคะ"){
+   $reply_message = 'ชื่อของฉันคือ BOTCAT';
+  }
+    
+     if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
+      $url = 'https://covid19.th-stat.com/api/open/today';
+   $json = file_get_contents($url);
+   $obj = json_decode($json);
+   $reply_message = 'ผู้ติดเชื้อสะสม '. $obj->Confirmed .' คน เสียชีวิต '.$obj->Deaths . ' คน' .' คน รักษาหายแล้ว '.$obj->Recovered . ' คน';
+ 
+         } 
+    
+    if($text =="@บอท ขอรหัสนิสิตของผู้พัฒนา ส่งไปที่ https://linebot.kantit.com/stuid.php"){
+        $url = 'https://linebot.kantit.com/stuid.php';
+     $ch = curl_init($url);
+     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+     $result = curl_exec($ch);
+     curl_close($ch);        
+     $obj = json_decode($result);     
+     $reply_message = $result;
+     //$reply_message = 'ผลการบันทึกข้อมูล'. $obj->{'status'} .' และ '.$obj->{'data'} . ' OK!';
+    }
+    
+    if($text =="@บอท ขอรายชื่อนิสิตที่ส่งงาน LineBoT"){
+         $url = 'https://linebot.kantit.com/list.php';
+     
+     $reply_message = file_get_contents($url);   // Get request content
+     
+     //$request_array = json_decode($request, true);   // Decode JSON to Array
+     
+     //$result = file_get_contents($url);     
+     //$obj = json_decode($result);
+     //$reply_message = 'มีส่งงาน '. $obj->{'Confirmed'} .' คน ได้แก่...';
+     //$reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} .' คน รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
+     $reply_message = "โปรดรอสักครู่....";
+    }
+    
+      if($text =="@บอท ขอที่อยู่มทร.หน่อยจิ"){
+         $url = 'https://www.google.com/maps/place/Phra+Nakhon+Si+Ayutthaya/@14.3935691,100.237786,10z/data=!3m1!4b1!4m8!1m2!2m1!1smaps+google!3m4!1s0x30e2736f5bfd8f7f:0x1019237450c4860!8m2!3d14.3532128!4d100.5689599';
+     
+     $reply_message = $url;   // Get request content
 
-		   //$reply_message = $reply_message;
-	   }
-	   
-		//$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
+     //$reply_message = $reply_message;
+    }
+    
+  //$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
    }
    else
     $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
